6d95a1d0ea29868064da6656888bdfc339392fa2
Erwann <erwann.fady@gmail.com>
2021-10-18 15:33:17 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const socket = new WebSocket("ws://127.0.0.1:3000/webSocket", "protocoleOne");
+let connect = false;
+let data = {
+    speed1 : 0,
+    speed2 : 0,
+    direction1 : 0,
+    direction2 : 0,
+}
+
+socket.onopen = () => {
+    connect = true;
+}
+
+setInterval(() => {
+    if (connect) {
+        socket.send(JSON.stringify(data));
+    }
+}, 50);
+
+function changeDirection(d) {
+    switch (d) {
+        default:
+            data.direction1 = 0;
+            data.direction2 = 0;
+            data.speed1 = 0;
+            data.speed2 = 0;
+            break
+
+        case 0:
+            data.direction1 = 2;
+            data.direction2 = 2;
+            data.speed1 = 50;
+            data.speed2 = 50;
+            break
+
+        case 1:
+            data.direction1 = 2;
+            data.direction2 = 0;
+            data.speed1 = 50;
+            data.speed2 = 0;
+            break
+
+        case 2:
+            data.direction1 = 1;
+            data.direction2 = 1;
+            data.speed1 = 50;
+            data.speed2 = 50;
+            break
+
+        case 3:
+            data.direction1 = 0;
+            data.direction2 = 2;
+            data.speed1 = 50;
+            data.speed2 = 50;
+            break
+    }
+}
+
+
+
